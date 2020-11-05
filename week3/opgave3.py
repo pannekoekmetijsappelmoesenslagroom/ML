b64ca89867c4bba6d89d3019a63775f01d9e37d3
hilkojj <hilkojj@outlook.com>
2020-11-06 00:23:27 +0100

    iets trainen

--- week3/opgave3.py
+++ week3/opgave3.py
@@ -1,21 +1,71 @@
 from tensorflow import keras
+import tensorflow as tf
 
-# magische data invoeren
-# magie train_images, train_labels    6 deel = train  1 deel test 
+# https://www.tensorflow.org/tutorials/images/classification
+
+from pathlib import Path
+
+data_dir = "./Fundus-data"
+
+train_dataset = keras.preprocessing.image_dataset_from_directory(
+    data_dir,
+    labels="inferred",
+    label_mode="int",
+    color_mode="grayscale",
+    batch_size=32,
+    image_size=(75, 75),
+    shuffle=True,
+    seed=1234,
+    validation_split=1/6,
+    subset="training",
+    interpolation="bilinear",
+)
+validation_dataset = keras.preprocessing.image_dataset_from_directory(
+    data_dir,
+    labels="inferred",
+    label_mode="int",
+    color_mode="grayscale",
+    batch_size=32,
+    image_size=(75, 75),
+    shuffle=True,
+    seed=1234,
+    validation_split=1/6,
+    subset="validation",
+    interpolation="bilinear",
+)
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+train_ds = train_dataset.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = validation_dataset.cache().prefetch(buffer_size=AUTOTUNE)
+
+class_names = train_dataset.class_names
+print(class_names)
+num_classes = len(class_names)
 
 model = keras.Sequential(
     [
-        keras.layers.Dense(75*75, activation="relu", input_shape=(75*75,)),
-        keras.layers.Dense(256, activation="relu"),
-        keras.layers.Dense(39, activation="softmax"),
+        keras.layers.experimental.preprocessing.Rescaling(1./255, input_shape=(75, 75, 1)),
+
+        keras.layers.Conv2D(16, 3, padding='same', activation='relu'),
+        keras.layers.MaxPooling2D(),
+        keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
+        keras.layers.MaxPooling2D(),
+        keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
+        keras.layers.MaxPooling2D(),
+        keras.layers.Flatten(),
+        keras.layers.Dense(128, activation='relu'),
+        keras.layers.Dense(num_classes)
     ]
 )
 
 model.compile(
     optimizer="adam",
-    loss="sparse_categorical_crossentropy",
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
     metrics=["accuracy"]
 )
 
+model.summary()
 
-model.fit(train_images, train_labels, epochs=6)
+
+model.fit(train_dataset, validation_data=validation_dataset, epochs=10)
